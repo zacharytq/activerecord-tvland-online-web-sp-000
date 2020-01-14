e496196419ca1b6b0141d3340aee5da219700044
chic-page-6502 <zacharytq@gmail.com>
2020-01-14 02:33:50 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(attr)
+    self.network = Network.new(attr)
+  end
+
 end
