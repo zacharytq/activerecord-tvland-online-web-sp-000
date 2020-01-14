6dd3c9f158d60adc6196fe65011272b43673a66b
chic-page-6502 <zacharytq@gmail.com>
2020-01-14 02:00:37 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,7 +3,7 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
     create_table :characters do |t|
       t.string :name
       t.integer :actor_id
-      t.ingeter :show_id
+      t.integer :show_id
     end
   end
 end
