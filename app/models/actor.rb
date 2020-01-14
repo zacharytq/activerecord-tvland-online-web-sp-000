4d9020989089be638974992b8a1c5a54f9905d7a
chic-page-6502 <zacharytq@gmail.com>
2020-01-14 02:19:27 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,6 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    self.first_name + self.last_name
+    self.first_name + "  " + self.last_name
   end
 end
